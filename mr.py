84263e4c1710e7117d211949dd4e76f8c8c2a491
nahim143 <110436449+nahim143@users.noreply.github.com>
2022-08-03 12:54:19 +0600

    Create mr.py

---
+++ mr.py
@@ -0,0 +1,28 @@
+import os,time
+def clear():
+	os.system('clear')
+def banner():
+	clear()
+	print("""
+\033[1;37m    $$\      $$\           $$\   $$\           $$\       $$\               
+$$$\    $$$ |          $$$\  $$ |          $$ |      \__|              
+$$$$\  $$$$ | $$$$$$\  $$$$\ $$ | $$$$$$\  $$$$$$$\  $$\ $$$$$$\$$$$\  
+$$\$$\$$ $$ |$$  __$$\ $$ $$\$$ | \____$$\ $$  __$$\ $$ |$$  _$$  _$$\ 
+$$ \$$$  $$ |$$ |  \__|$$ \$$$$ | $$$$$$$ |$$ |  $$ |$$ |$$ / $$ / $$ |
+$$ |\$  /$$ |$$ |      $$ |\$$$ |$$  __$$ |$$ |  $$ |$$ |$$ | $$ | $$ |
+$$ | \_/ $$ |$$ |$$\   $$ | \$$ |\$$$$$$$ |$$ |  $$ |$$ |$$ | $$ | $$ |
+\__|     \__|\__|\__|  \__|  \__| \_______|\__|  \__|\__|\__| \__| \__|
+(!)========================================
+(!) Author   : Mr.NAHIM (Mamun Sikder)
+(!) Guthub   : Nahim143
+(!) Facebook : Chand Muhammad Khan 
+(!) Type     : FREE
+\033[1;37m(!)========================================""")
+def Update():
+    banner()
+    print("\n\x1b[1;92m Updating Soon :-/")
+    print(' Please Wait For Update 😊 ')
+    print(' Join Over Facebook Group')
+    os.system('xdg-open https://www.facebook.com/groups/cyber.420.the.silent.killers.official');time.sleep(3)
+    input('')
+Update()
